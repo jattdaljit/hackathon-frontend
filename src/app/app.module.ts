a6fb7ed452a5b49002c12c9bb7e0ab7c0ad11c60
Daljit Singh <Daljit_Singh@epam.com>
2019-09-10 18:35:37 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { ResultComponent } from './result/result.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    ResultComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
